6d20ddb4791fe8438339cc3464187c7c50dfaaf9
Im1x <im1xdev@gmail.com>
2024-03-08 18:57:21 +0300

    Add logo

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,16 @@
 import NavBar from "../components/Navbar";
 import SongRequest from "../components/songRequest";
+import logo from "../assets/logo.jpg";
+import {Center} from "@mantine/core";
 const Main = () => {
   return (
   <div style={{display: "flex"}}>
-    <NavBar/>
+    <div>
+      <Center>
+        <img src={logo} alt="logo" />
+      </Center>
+      <NavBar/>
+    </div>
     <SongRequest/>
   </div>
   );
