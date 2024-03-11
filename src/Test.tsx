78277c777a108704ba7cc806b069b62134978322
Im1x <im1xdev@gmail.com>
2024-03-11 18:39:40 +0300

    Remove unnecessary images

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { authApi } from "./services/AuthService.ts";
 import { IRegistration } from "./models/Registration.ts";
@@ -43,10 +41,8 @@ function App() {
     <>
       <div className="flex items-center justify-center">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <div>
